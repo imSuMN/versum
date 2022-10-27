aef594f23f1d79a2012d5e8b5afce2b7653e2c8c
Suman Kumar <suman.kumar.it.2020@tint.edu.in>
2022-10-27 22:44:29 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>This is suman kumar</h1>
+    </div>
+  );
+}
